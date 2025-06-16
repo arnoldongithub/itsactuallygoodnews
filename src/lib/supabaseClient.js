b93fa5f8fade63f3a7c9a52238f712290f95ed41
arnoldongithub <arnoldkmbuthia@icloud.com>
2025-06-16 19:36:27 +0300

    fix: install vite locally and update build setup

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,4 +1,4 @@
-import { createClient } from '@supabase/supabase-js'
+import { supabase } from "./supabaseClient.js"
 
 const supabaseUrl = 'https://qcdapfobdsqvzmagimvr.supabase.co'
 const supabaseKey = 'YOeyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFjZGFwZm9iZHNxdnptYWdpbXZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTAwNzE0MDEsImV4cCI6MjA2NTY0NzQwMX0.VLIavDgWnoNA-4LLfKZ7P6kGAW-YiASz50dFXyX_MCw'

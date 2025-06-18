a0383908ad5f6c9bed0fd1ac4ed48dbd9e67d47d
arnoldongithub <arnoldkmbuthia@icloud.com>
2025-06-18 12:34:44 +0300

    Integrate RSS auto-import system into main app

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,7 @@
+// Starts the RSS auto-import system
+import './lib/news-import-setup.js';
 
+// Other existing imports
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router } from 'react-router-dom';
